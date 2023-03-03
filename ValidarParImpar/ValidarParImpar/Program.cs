67e83c52f2d25f0b9b35c64d559ffcae0ccdd26b
Juan Pablo Rebolledo Almeida <rebolledoalmeida@hotmail.com>
2023-03-03 03:36:56 -0500

    Programa para Validar si un numero es par o impar

---
+++ ValidarParImpar/ValidarParImpar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ValidarParImpar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            Console.WriteLine("Programa para validar si un numero es par o impar");
+            Console.WriteLine("--------------------------------------------------");
+ 
+
+            Console.WriteLine("Ingrese un numero positivo");
+            num1 = Convert.ToInt16(Console.ReadLine());
+
+                
+            if(num1 < 0) 
+            {
+                while(num1 < 0)
+                    {
+                        Console.WriteLine("Ha ingresado un numero negativo, porfavor ingrese uno positivo");
+                        num1 = Convert.ToInt16(Console.ReadLine());
+                    }
+            }
+            
+            if(num1 % 2 == 0)
+            {
+                Console.WriteLine($"el numero {num1} es par");
+            }
+            else
+            {
+                Console.WriteLine($"El numero {num1} es impar");
+            }
+        }
+    }
+}
